7b9c1a5d70f26fffe85dc0ec26d323231d211b3e
hupingrong <842300836@qq.com>
2021-05-21 16:50:56 +0800

    hr

---
+++ LayUIDemo/LayUIDemo/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LayUIDemo.Models;
+
+namespace LayUIDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetAll(int page, int limit)
+        {
+            List<StuInfo> stulist = new List<StuInfo>();
+            using (DBEntities db = new DBEntities())
+            {
+                stulist = db.StuInfo.OrderBy(x => x.StuID).ToList();
+            }
+            
+            var redata = new {
+                code = 0,
+                count = stulist.Count(),
+                data = stulist.Skip((page - 1) * limit).Take(limit).ToList(),
+                msg = "sss"
+            };
+            //var redata = new ReJson<StuInfo>(){
+            //   code = 0,
+            //   count = stulist.Count(),
+            //   data = stulist.Skip((page - 1) * limit).Take(limit).ToList(),
+            //    msg = "sss"
+            //};
+            return Json(redata, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult AddStu()
+        {
+            return View();
+        }
+
+        public ActionResult GetClassList()
+        {
+            List<ClassInfo> list = new List<ClassInfo>();
+            using (DBEntities db = new DBEntities())
+            {
+                list = db.ClassInfo.ToList();
+            }
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ LayUIDemo/LayUIDemo/Models/JsonRe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LayUIDemo.Models
+{
+    public class ReJson<T>
+    {
+        public int code { set; get; }
+        public int count { set; get; }
+        public List<T> data { set; get; }
+        public string msg { set; get; }
+    }
+}
